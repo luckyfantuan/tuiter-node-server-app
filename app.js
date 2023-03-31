213ecff57afd5a2a98ab09aca6658d5808aeee56
Lili Qing <lili.lily.qing@gmail.com>
2023-03-31 11:11:15 -0700

    a9 add connection string

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ import TuitsController
 import cors from 'cors'
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/tuiter'
+
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json());
